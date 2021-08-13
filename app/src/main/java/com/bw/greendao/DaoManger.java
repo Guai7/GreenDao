c8753c12879e4956ef6424f8ba85b0bdc271a866
王益德 <172655102@qq.com>
2021-08-13 11:13:25 +0800

    新增数据库

---
+++ app/src/main/java/com/bw/greendao/DaoManger.java
@@ -0,0 +1,10 @@
+package com.bw.greendao;
+public
+/**
+*GreenDao
+*file name is : DaoManger
+*created by Ender on 2021/8/13 11:04.
+*author : 王益德
+*Describe:
+*/   class DaoManger   {
+}
---
+++ app/src/main/java/com/bw/greendao/Student.java
@@ -0,0 +1,10 @@
+package com.bw.greendao;
+public
+/**
+*GreenDao
+*file name is : Sutdent
+*created by Ender on 2021/8/13 11:01.
+*author : 王益德
+*Describe:
+*/   class Sutdent   {
+}
